b1775c62f3632c1c885975a783e345387c1585b4
Mateusz <Mateusz@DESKTOP-DIM2CTD>
2020-10-20 19:30:32 +0200

    Dodaj pliki .gitignore i .gitattributes.

---
+++ ProfileView.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Konkurs
+{
+    class ProfileView
+    {
+        public string plec;
+        public int waga_poczatkowa, wzrost, wiek;
+
+        public double BMI, Calorie;
+
+        public void Profile(int waga_poczatkowa, int wzrost, int wiek, string plec)
+        {
+            this.waga_poczatkowa = waga_poczatkowa;
+            this.wzrost = wzrost;
+            this.wiek = wiek;
+            this.plec = plec;
+        }
+
+        public double LiczBMI(int waga, double wzrost) 
+        {
+            wzrost = wzrost / 100;
+            BMI = (waga / Math.Pow(wzrost,2));
+
+            return Math.Round(BMI,2);
+        }
+
+        public double LiczZapotrzebowanie(string plec,int masa_ciala, int wzrost, int wiek, string stopie_aktywnosci) 
+        {
+
+            if(plec == "Kobieta") 
+            {
+                switch (stopie_aktywnosci)
+                {
+                    case "Niski":
+                        Calorie = (655 + (9.6 * masa_ciala) + (1.8 * wzrost) - (4.7 * wiek)) * 1.4;
+                        break;
+                    case "Średni":
+                        Calorie = (655 + (9.6 * masa_ciala) + (1.8 * wzrost) - (4.7 * wiek)) * 1.7;
+                        break;
+                    case "Wysoki":
+                        Calorie = (655 + (9.6 * masa_ciala) + (1.8 * wzrost) - (4.7 * wiek)) * 2.2;
+                        break;
+                }
+            }
+            else 
+            {
+                switch (stopie_aktywnosci)
+                {
+                    case "Niski":
+                        Calorie = (66 + (13.7 * masa_ciala) + (5 * wzrost) - (6.76 * wiek)) * 1.4;
+                        break;
+                    case "Średni":
+                        Calorie = (66 + (13.7 * masa_ciala) + (5 * wzrost) - (6.76 * wiek)) * 1.7;
+                        break;
+                    case "Wysoki":
+                        Calorie = (66 + (13.7 * masa_ciala) + (5 * wzrost) - (6.76 * wiek)) * 2.2;
+                        break;
+                }
+            }
+
+            return Math.Round(Calorie,0);
+        }
+    }
+}
---
+++ Register.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Konkurs
+{
+    [Activity(Label = "Register")]
+    public class Register : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.register_view);
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            toolbar.Title = "Rejestracja";
+            SetActionBar(toolbar);
+
+            EditText _imie = FindViewById<EditText>(Resource.Id.imie);
+            EditText _wiek = FindViewById<EditText>(Resource.Id.wiek);
+            EditText _waga = FindViewById<EditText>(Resource.Id.waga);
+            EditText _wzrost = FindViewById<EditText>(Resource.Id.wzrost);
+
+            Button button = FindViewById<Button>(Resource.Id.submit_button);
+            Spinner spinner = FindViewById<Spinner>(Resource.Id.spinner);
+            spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            var adapter = ArrayAdapter.CreateFromResource(
+                this, Resource.Array.gender, Android.Resource.Layout.SimpleSpinnerItem);
+
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
+            button.Click += (sender, e) =>
+            {
+                if (_imie.Text.Length != 0)
+                {
+                    if (_wiek.Text.ToString() != "")
+                    {
+                        if (_waga.Text.ToString() != "")
+                        {
+                            if (_wzrost.Text.ToString() != "")
+                            {
+                                var _login = Application.Context.GetSharedPreferences("Login", FileCreationMode.Private).Edit();
+                                _login.PutString("Name", _imie.Text).Apply();
+                                _login.PutString("Weight", _waga.Text).Apply();
+                                _login.PutString("Height", _wzrost.Text).Apply();
+                                _login.PutString("Age", _wiek.Text).Apply();
+                                var _register = Application.Context.GetSharedPreferences("isRegister", FileCreationMode.Private);
+                                _register.Edit().PutBoolean("Register", true).Apply();
+                                var intent = new Intent(this, typeof(MainActivity));
+                                StartActivity(intent);
+                                Finish();
+                            }
+                            else Android.Widget.Toast.MakeText(this, "Podaj swój wzrost", ToastLength.Short).Show();
+                        }
+                        else Android.Widget.Toast.MakeText(this, "Podaj swoją wage!", ToastLength.Short).Show();
+                    }
+                    else Android.Widget.Toast.MakeText(this, "Podaj wiek!", ToastLength.Short).Show();
+                }
+                else Android.Widget.Toast.MakeText(this, "Podaj Imię!", ToastLength.Short).Show();
+            };
+        }
+
+        private void spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            Spinner spinner = (Spinner)sender;
+            var _login = Application.Context.GetSharedPreferences("Login", FileCreationMode.Private).Edit();
+            _login.PutString("Gender", spinner.GetItemAtPosition(e.Position).ToString()).Apply();
+        }
+    }
+}
